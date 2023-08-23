5069e30f8980b03500a8fc4edc87c6ebf22ff761
iznaut <36773942+iznaut@users.noreply.github.com>
2023-08-23 11:44:40 -0500

    fix for close button

--- src/main.gd
+++ src/main.gd
@@ -1,19 +1,12 @@
 extends Node
 
 
-signal close_requested
-
-const ASSISTANT_SCENE := preload("res://src/assistant/dorkus_assistant.tscn")
-
-
 func _ready():
 	if not Utility.does_config_exist():
 		var content = FileAccess.get_file_as_string("res://config_template.ini")
 		var new_config = FileAccess.open(Utility.get_config_path(), FileAccess.WRITE)
 		new_config.store_string(content)
 		new_config.close()
-
-	ASSISTANT_SCENE.instantiate()
 
 
 # TODO i just kinda gave up here lmao
@@ -26,7 +19,12 @@ func _notification(what):
 				get_tree().quit()
 		)
 
-		$OBSHelper.send_command("StopRecord")
+		if $OBSHelper.ready_to_close:
+			OS.kill($OBSHelper.app_process_id)
+			get_tree().quit()
+		else:
+			$OBSHelper.send_command("StopRecord")
+
 
 		# var helpers = []
 
--- src/windows/close_button.gd
+++ src/windows/close_button.gd
@@ -2,5 +2,5 @@ extends Button
 
 
 func _on_pressed():
-	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_window().propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 	disabled = true
